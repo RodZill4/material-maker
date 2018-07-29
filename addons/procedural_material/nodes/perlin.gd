7631f4489bafe8be3177d2ef5eaaa8410b45c8fa
RodZill4 <rodzilla@free.fr>
2018-07-29 14:21:49 +0200

    Smart typo in perlin noise node

--- addons/procedural_material/nodes/perlin.gd
+++ addons/procedural_material/nodes/perlin.gd
@@ -4,16 +4,16 @@ extends "res://addons/procedural_material/node_base.gd"
 var scale_x
 var scale_y
 var iterations
-var persistance
+var persistence
 
 func _ready():
 	set_slot(0, false, 0, Color(0.5, 0.5, 1), true, 0, Color(0.5, 0.5, 1))
-	initialize_properties([ $GridContainer/scale_x, $GridContainer/scale_y, $GridContainer/iterations, $GridContainer/persistance ])
+	initialize_properties([ $GridContainer/scale_x, $GridContainer/scale_y, $GridContainer/iterations, $GridContainer/persistence ])
 
 func _get_shader_code(uv):
 	var rv = { defs="", code="" }
 	if generated_variants.empty():
-		rv.defs = "float "+name+"_f(vec2 uv) { return perlin(uv, vec2(%f, %f), %d, %.9f, %d); }\n" % [ scale_x, scale_y, iterations, persistance, get_seed() ]
+		rv.defs = "float "+name+"_f(vec2 uv) { return perlin(uv, vec2(%f, %f), %d, %.9f, %d); }\n" % [ scale_x, scale_y, iterations, persistence, get_seed() ]
 	var variant_index = generated_variants.find(uv)
 	if variant_index == -1:
 		variant_index = generated_variants.size()
