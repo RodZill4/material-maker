477eec692bfbe29bd309bacedd1cca81344612eb
RodZill4 <rodzilla@free.fr>
2019-10-13 22:06:13 +0200

    Fixed normal map and updated 3d preview

--- addons/material_maker/preview.gd
+++ addons/material_maker/preview.gd
@@ -10,24 +10,35 @@ const ENVIRONMENTS = [
 onready var objects = $MaterialPreview/Preview3d/Objects
 onready var current_object = objects.get_child(0)
 
+onready var environments = $MaterialPreview/Preview3d/Environments
+onready var current_environment = environments.get_child(0)
+
 signal need_update
 signal show_background_preview
 
 func _ready():
-	current_object.visible = true
 	$Config/Model.clear()
 	for o in objects.get_children():
 		var m = o.get_surface_material(0)
 		o.set_surface_material(0, m.duplicate())
 		$Config/Model.add_item(o.name)
+	call_deferred("_on_Model_item_selected", 0)
+	$Config/Environment.clear()
+	for e in environments.get_children():
+		$Config/Environment.add_item(e.name)
+	call_deferred("_on_Environment_item_selected", 0)
 	$MaterialPreview/Preview3d/ObjectRotate.play("rotate")
 	$Preview2D.material = $Preview2D.material.duplicate(true)
-	_on_Environment_item_selected($Config/Environment.selected)
 	_on_Preview_resized()
 	$MaterialPreview/Preview3d/CameraPivot/Camera/RemoteTransform.set_remote_node("../../../../../../../ProjectsPane/BackgroundPreview/Viewport/Camera")
 
 func _on_Environment_item_selected(id):
-	$MaterialPreview/Preview3d/WorldEnvironment.environment.background_sky.panorama = load("res://addons/material_maker/panoramas/"+ENVIRONMENTS[id]+".hdr")
+	current_environment.visible = false
+	current_environment = environments.get_child(id)
+	print(current_environment.environment)
+	$MaterialPreview/Preview3d/CameraPivot/Camera.set_environment(current_environment.environment)
+	get_node("../../ProjectsPane/BackgroundPreview/Viewport/Camera").set_environment(current_environment.environment)
+	current_environment.visible = true
 
 func _on_Model_item_selected(id):
 	current_object.visible = false
@@ -62,6 +73,9 @@ func on_gui_input(event):
 	if event is InputEventMouseButton:
 		$MaterialPreview/Preview3d/ObjectRotate.stop()
 	elif event is InputEventMouseMotion:
-		if event.button_mask != 0:
+		if event.button_mask & BUTTON_MASK_LEFT:
 			$MaterialPreview/Preview3d/Objects.rotation.y += 0.01*event.relative.x
+			$MaterialPreview/Preview3d/Objects.rotation.x += 0.01*event.relative.y
+		elif event.button_mask & BUTTON_MASK_RIGHT:
+			$MaterialPreview/Preview3d/CameraPivot.rotation.y += 0.01*event.relative.x
 			$MaterialPreview/Preview3d/CameraPivot.rotation.x -= 0.01*event.relative.y
