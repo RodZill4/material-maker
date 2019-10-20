d376aa22f8dedfd437f2bb48788382f2d55fbc89
RodZill4 <rodzilla@free.fr>
2019-10-20 22:37:42 +0200

    Updated convolution node and added gaussian blur

--- addons/material_maker/engine/gen_convolution.gd
+++ addons/material_maker/engine/gen_convolution.gd
@@ -94,41 +94,37 @@ func _get_shader_code(uv : String, output_index : int, context : MMGenContext):
 							sum[0] += coef
 							sum[1] += coef
 							sum[2] += coef
-							if convolution_params.input_type == "f":
-								coef = "vec3(%.9f)" % [ coef ]
-							elif convolution_params.input_type == "rgb":
-								coef = "%.9f" % coef
-							else:
+							coef = [ coef, coef, coef ]
+							if convolution_params.input_type != "f" and convolution_params.input_type != "rgb":
 								errors += 1
-						if typeof(coef) == TYPE_ARRAY and coef.size() == 3:
+						elif typeof(coef) == TYPE_ARRAY and coef.size() == 3:
 							if convolution_params.input_type == "f" or convolution_params.input_type == "rgb":
 								sum[0] += coef[0]
 								sum[1] += coef[1]
 								sum[2] += coef[2]
-								coef = "vec3(%.9f,%.9f,%.9f)" % [ coef[0], coef[1], coef[2] ]
 							else:
 								errors += 1
+						else:
+							errors += 1
 					"rgba":
 						if typeof(coef) == TYPE_REAL:
 							sum[0] += coef
 							sum[1] += coef
 							sum[2] += coef
 							sum[3] += coef
-							if convolution_params.input_type == "f":
-								coef = "vec4(%.9f)" % [ coef ]
-							if convolution_params.input_type == "rgba":
-								coef = "%.9f" % coef
-							else:
+							coef = [ coef, coef, coef, coef ]
+							if convolution_params.input_type != "f" and convolution_params.input_type != "rgba":
 								errors += 1
-						if typeof(coef) == TYPE_ARRAY and coef.size() == 3:
+						elif typeof(coef) == TYPE_ARRAY and coef.size() == 4:
 							if convolution_params.input_type == "f" or convolution_params.input_type == "rgba":
 								sum[0] += coef[0]
 								sum[1] += coef[1]
 								sum[2] += coef[2]
 								sum[3] += coef[3]
-								coef = "vec4(%.9f,%.9f,%.9f,%.9f)" % [ coef[0], coef[1], coef[2], coef[3] ]
 							else:
 								errors += 1
+						else:
+							errors += 1
 				line.push_back(coef)
 			matrix.push_back(line)
 		# Generate code
@@ -136,11 +132,16 @@ func _get_shader_code(uv : String, output_index : int, context : MMGenContext):
 		if errors > 0:
 			pass
 		else:
+			if convolution_params.has("normalized") and convolution_params.normalized:
+				for i in range(sum.size()):
+					sum[i] = 1.0/sum[i]
+			else:
+				sum = [ 1.0, 1.0, 1.0, 1.0 ]
 			for dy in range(-convolution_params.y, convolution_params.y+1):
 				var line = matrix[dy+convolution_params.y]
 				for dx in range(-convolution_params.x, convolution_params.x+1):
 					var coef = line[dx+convolution_params.x]
-					var uv_str = "((%s)+vec2(%.9f,%.9f))" % [ uv, dx*epsilon, dy*epsilon ]
+					var uv_str = "fract((%s)+vec2(%.9f,%.9f))" % [ uv, dx*epsilon, dy*epsilon ]
 					var src_code = source.generator.get_shader_code(uv_str, source.output_index, context)
 					while src_code is GDScriptFunctionState:
 						src_code = yield(src_code, "completed")
@@ -152,21 +153,17 @@ func _get_shader_code(uv : String, output_index : int, context : MMGenContext):
 					rv.defs += src_code.defs
 					# Add generated code
 					rv.code += src_code.code
-					rv.code += "%s_%d += %s*%s;\n" % [ genname, variant_index, coef, src_code[convolution_params.input_type] ]
+					var coef_str : String
+					match convolution_params.output_type:
+						"f":
+							coef_str = "%.9f" % [ coef[0] * sum[0] ]
+						"rgb":
+							coef_str = "vec3(%.9f, %.9f, %.9f)" % [ coef[0] * sum[0], coef[1] * sum[1], coef[2] * sum[2] ]
+						"rgba":
+							coef_str = "vec4(%.9f, %.9f, %.9f, %.9f)" % [ coef[0] * sum[0], coef[1] * sum[1], coef[2] * sum[2], coef[3] * sum[3] ]
+					rv.code += "%s_%d += %s*%s;\n" % [ genname, variant_index, coef_str, src_code[convolution_params.input_type] ]
 					for t in src_code.textures.keys():
 						rv.textures[t] = src_code.textures[t]
-			# normalize
-			if false:
-				var sum_str : String
-				var count : float = float((2*convolution_params.x+1) / (2*convolution_params.y+1))
-				match convolution_params.output_type:
-					"f":
-						sum_str = "%.9f" % [ sum[0] / count ]
-					"rgb":
-						sum_str = "vec3(%.9f, %.9f, %.9f)" % [ sum[0] / count, sum[1] / count, sum[2] / count ]
-					"rgba":
-						sum_str = "vec4(%.9f, %.9f, %.9f, %.9f)" % [ sum[0] / count, sum[1] / count, sum[2] / count, sum[3] / count ]
-				rv.code += "%s_%d /= %s;\n" % [ genname, variant_index, sum_str ]
 			rv[convolution_params.output_type] = "%s_%d" % [ genname, variant_index ]
 	return rv
 
