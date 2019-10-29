4c2274104bc7db0a0f4e1acace3a543bd585a5e0
RodZill4 <rodzilla@free.fr>
2019-10-29 22:10:08 +0100

    Made IOs node editable (can add/remove ports)

---
+++ addons/material_maker/nodes/ios/port.gd
@@ -0,0 +1,23 @@
+extends HBoxContainer
+
+func set_label(l : String) -> void:
+	$Name.set_text(l)
+
+func update_up_down_button() -> void:
+	var parent = get_parent()
+	if parent == null:
+		return
+	$Up.disabled = (get_index() == 0)
+	$Down.disabled = (get_index() == get_parent().get_child_count()-2)
+
+func _on_Name_label_changed(new_label):
+	get_parent().generator.set_port_name(get_index(), new_label)
+
+func _on_Delete_pressed():
+	get_parent().generator.delete_port(get_index())
+
+func _on_Up_pressed():
+	get_parent().generator.swap_ports(get_index(), get_index()-1)
+
+func _on_Down_pressed():
+	get_parent().generator.swap_ports(get_index(), get_index()+1)
