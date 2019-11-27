8edd21e48877d8fb20cddee9a1f5138a79eff3f9
RodZill4 <rodzilla@free.fr>
2019-11-27 07:45:58 +0100

    Added new examples to test splatter node and material workflow

--- addons/material_maker/nodes/generic.gd
+++ addons/material_maker/nodes/generic.gd
@@ -126,6 +126,7 @@ func create_parameter_control(p : Dictionary) -> Control:
 		control = CheckBox.new()
 	elif p.type == "color":
 		control = ColorPickerButton.new()
+		control.rect_min_size.x = 40
 	elif p.type == "gradient":
 		control = preload("res://addons/material_maker/widgets/gradient_editor.tscn").instance()
 	elif p.type == "string":
