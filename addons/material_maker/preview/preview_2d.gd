9a4800883f55a2f096644ab657092b7b795e3678
RodZill4 <rodzilla@free.fr>
2019-12-10 08:00:55 +0100

    updated 2d view to show texture as square with margins

--- addons/material_maker/preview/preview_2d.gd
+++ addons/material_maker/preview/preview_2d.gd
@@ -1,7 +1,8 @@
+tool
 extends ColorRect
-
-func _ready() -> void:
-	material = material.duplicate(true)
 
 func set_preview_texture(tex: Texture) -> void:
 	material.set_shader_param("tex", tex)
+
+func on_resized() -> void:
+	material.set_shader_param("size", rect_size)
