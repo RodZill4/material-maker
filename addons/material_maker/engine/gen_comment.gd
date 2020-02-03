6338680b32143a4caf3027fd44a8c77f9d890155
RodZill4 <rodzilla@free.fr>
2020-02-03 20:55:32 +0100

    Added color and editable title to comment nodes

--- addons/material_maker/engine/gen_comment.gd
+++ addons/material_maker/engine/gen_comment.gd
@@ -8,6 +8,8 @@ Comments to put in the graph
 
 var text : String = "Double-click to write a comment"
 var size : Vector2 = Vector2(0, 0)
+var title : String = "Comment"
+var color : Color = Color(1.0, 0.5, 0.0)
 
 func _ready() -> void:
 	if !parameters.has("size"):
@@ -30,6 +32,8 @@ func get_output_defs() -> Array:
 
 func _serialize(data: Dictionary) -> Dictionary:
 	data.type = "comment"
+	data.title = title
+	data.color = MMType.serialize_value(color)
 	data.text = text
 	data.size = { x=size.x, y=size.y }
 	return data
@@ -39,3 +43,7 @@ func _deserialize(data : Dictionary) -> void:
 		text = data.text
 	if data.has("size"):
 		size = Vector2(data.size.x, data.size.y)
+	if data.has("title"):
+		title = data.title
+	if data.has("color"):
+		color = MMType.deserialize_value(data.color)
