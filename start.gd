c53b4e06eb5d1f1731085b829cba31b4a821173c
RodZill4 <rodzilla@free.fr>
2020-02-06 21:46:49 +0100

    updated the start scene to perform background loading

--- start.gd
+++ start.gd
@@ -1,10 +1,27 @@
 extends Control
 
-func _ready():
-	call_deferred("change_scene")
+var loader
 
-func change_scene():
+func _ready():
+	var path : String
 	if Directory.new().file_exists("res://material_maker/main_window.tscn"):
-		get_tree().change_scene("res://material_maker/main_window.tscn")
+		path = "res://material_maker/main_window.tscn"
 	else:
-		get_tree().change_scene("res://demo/demo.tscn")
+		path = "res://demo/demo.tscn"
+	loader = ResourceLoader.load_interactive(path)
+	if loader == null: # check for errors
+		print("error")
+		queue_free()
+
+func _process(delta):
+	var err = loader.poll()
+	if err == ERR_FILE_EOF:
+		var resource = loader.get_resource()
+		get_node("/root").add_child(resource.instance())
+		queue_free()
+	elif err == OK:
+		var progress = float(loader.get_stage()) / loader.get_stage_count()
+		$VBoxContainer/ProgressBar.value = 100.0*progress
+	else: # error during loading
+		print("error")
+		queue_free()
