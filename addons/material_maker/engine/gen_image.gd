1860036c555bc9173bc2cfd6dc49ab539fd75e6d
Rodolphe Suescun <rodzilla@free.fr>
2020-08-23 12:39:12 +0200

    Added timer to image node to check if fle changed (#152)

--- addons/material_maker/engine/gen_image.gd
+++ addons/material_maker/engine/gen_image.gd
@@ -2,9 +2,18 @@ tool
 extends MMGenTexture
 class_name MMGenImage
 
+var timer : Timer
+
 """
 Texture generator from image
 """
+
+func _ready() -> void:
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 5
+	timer.start()
+	timer.connect("timeout", self, "_on_timeout")
 
 func get_type() -> String:
 	return "image"
@@ -19,3 +28,7 @@ func set_parameter(n : String, v) -> void:
 	.set_parameter(n, v)
 	if n == "image":
 		texture.load(v)
+
+func _on_timeout():
+	set_parameter("image", get_parameter("image"))
+	
