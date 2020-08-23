a72cc40bfb6acb2f09be1d316db30764bd97b2a4
Rodolphe Suescun <rodzilla@free.fr>
2020-08-23 22:23:59 +0200

    Check image file modified time before updating the texture in image nodes (#152)

--- addons/material_maker/engine/gen_image.gd
+++ addons/material_maker/engine/gen_image.gd
@@ -3,6 +3,7 @@ extends MMGenTexture
 class_name MMGenImage
 
 var timer : Timer
+var filetime : int = 0
 
 """
 Texture generator from image
@@ -11,7 +12,7 @@ Texture generator from image
 func _ready() -> void:
 	timer = Timer.new()
 	add_child(timer)
-	timer.wait_time = 5
+	timer.wait_time = 2
 	timer.start()
 	timer.connect("timeout", self, "_on_timeout")
 
@@ -24,11 +25,22 @@ func get_type_name() -> String:
 func get_parameter_defs() -> Array:
 	return [ { name="image", type="path" } ]
 
+func get_filetime(file_path : String) -> int:
+	var f : File = File.new()
+	if f.file_exists(file_path):
+		return f.get_modified_time(file_path)
+	return 0
+
 func set_parameter(n : String, v) -> void:
 	.set_parameter(n, v)
 	if n == "image":
+		filetime = get_filetime(v)
 		texture.load(v)
 
-func _on_timeout():
-	set_parameter("image", get_parameter("image"))
-	
+func _on_timeout() -> void:
+	var file_path : String = get_parameter("image")
+	var new_filetime : int = get_filetime(file_path)
+	if filetime != new_filetime:
+		filetime = new_filetime
+		texture.load(file_path)
+		.set_parameter("image", file_path)
