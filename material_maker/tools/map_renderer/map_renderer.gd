de3a76c8c877e6d72db0a78af96216a74c8afc3c
RodZill4 <rodzilla@free.fr>
2021-01-09 09:46:53 +0100

    Added progress window and preferences to AO/Thickness map baking

--- material_maker/tools/map_renderer/map_renderer.gd
+++ material_maker/tools/map_renderer/map_renderer.gd
@@ -11,10 +11,6 @@ export(ShaderMaterial) var dilate_pass2
 export(ShaderMaterial) var seams_pass1
 export(ShaderMaterial) var seams_pass2
 
-var ray_count = 64
-var ao_ray_dist = 128.0
-var denoise_radius = 3
-
 func _ready():
 	pass
 
@@ -23,8 +19,8 @@ func gen(mesh: Mesh, map : String, renderer_method : String, arguments : Array, 
 		mesh_normal = { first=mesh_normal_material, second=dilate_pass1, third=dilate_pass2 },
 		inv_uv =      { first=inv_uv_material, second=dilate_pass1, third=dilate_pass2 },
 		curvature =   { first=curvature_material, second=dilate_pass1, third=dilate_pass2 },
-		thickness =   { first=ao_material, second=dilate_pass1, third=dilate_pass2 },
-		ao =          { first=ao_material, second=dilate_pass1, third=dilate_pass2 },
+		thickness =   { first=ao_material, second=dilate_pass1, third=dilate_pass2, map_name="Thickness" },
+		ao =          { first=ao_material, second=dilate_pass1, third=dilate_pass2, map_name="Ambient Occlusion" },
 		seams =       { first=white_material, second=seams_pass1, third=seams_pass2 }
 	}
 	var passes = bake_passes[map]
@@ -37,6 +33,14 @@ func gen(mesh: Mesh, map : String, renderer_method : String, arguments : Array, 
 	
 	var aabb = $MeshInstance.get_aabb()
 	if map in ["ao", "thickness"]:
+		var main_window = get_node("/root/MainWindow")
+		var ray_count = main_window.get_config("bake_ray_count")
+		var ao_ray_dist = main_window.get_config("bake_ao_ray_dist")
+		var denoise_radius = main_window.get_config("bake_denoise_radius")
+		var progress_dialog = preload("res://material_maker/windows/progress_window/progress_window.tscn").instance()
+		progress_dialog.set_text("Generating "+passes.map_name+" map")
+		progress_dialog.set_progress(0)
+		main_window.add_child(progress_dialog)
 		var ray_distance = ao_ray_dist
 		if map == "thickness":
 			ray_distance = -aabb.size.length()
@@ -44,6 +48,7 @@ func gen(mesh: Mesh, map : String, renderer_method : String, arguments : Array, 
 		ao_material.set_shader_param("bvh_data", bvh_data)
 		ao_material.set_shader_param("max_dist", ray_distance)
 		for i in ray_count:
+			progress_dialog.set_progress(float(i)/ray_count)
 			ao_material.set_shader_param("iteration", i+1)
 			render_target_update_mode = Viewport.UPDATE_ONCE
 			yield(get_tree(), "idle_frame")
@@ -53,6 +58,7 @@ func gen(mesh: Mesh, map : String, renderer_method : String, arguments : Array, 
 		render_target_update_mode = Viewport.UPDATE_ONCE
 		yield(get_tree(), "idle_frame")
 		yield(get_tree(), "idle_frame")
+		progress_dialog.queue_free()
 	else:
 		inv_uv_material.set_shader_param("position", aabb.position)
 		inv_uv_material.set_shader_param("size", aabb.size)
---
+++ material_maker/windows/progress_window/progress_window.gd
@@ -0,0 +1,10 @@
+extends Popup
+
+func _ready() -> void:
+	popup_centered()
+
+func set_text(t) -> void:
+	$PanelContainer/VBoxContainer/Step.text = t
+
+func set_progress(p) -> void:
+	$PanelContainer/VBoxContainer/ProgressBar.value = p * 100.0
