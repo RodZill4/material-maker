35154d71925624fba404d9020276844fcb8b7767
RodZill4 <rodzilla@free.fr>
2021-01-12 08:54:36 +0100

    Updated curve parameters (now bezier curves)

--- material_maker/widgets/curve_edit/curve_view.gd
+++ material_maker/widgets/curve_edit/curve_view.gd
@@ -24,17 +24,21 @@ func _draw():
 			draw_line(Vector2(0, p.y), Vector2(rect_size.x-1, p.y), Color(0.25, 0.25, 0.25))
 	for i in range(curve.points.size()-1):
 		var p1 = curve.points[i].p
-		var s1 = curve.points[i].rs
 		var p2 = curve.points[i+1].p
-		var s2 = curve.points[i+1].ls
+		var d = (p2.x-p1.x)/3.0
+		var yac = p1.y+d*curve.points[i].rs
+		var ybc = p2.y-d*curve.points[i+1].ls
 		var p = transform_point(p1)
 		var count : int = max(1, int((transform_point(p2).x-p.x/5.0)))
-		for t in range(count):
-			var tt = (t+1.0)/count
-			var x = p1.x+(p2.x-p1.x)*tt
-			var y1 = p1.y+(x-p1.x)*s1
-			var y2 = p2.y+(x-p2.x)*s2
-			var np = transform_point(Vector2(x, lerp(y1, y2, (3.0-2.0*tt)*tt*tt)))
+		for tt in range(count):
+			var t = (tt+1.0)/count
+			var omt = (1.0 - t)
+			var omt2 = omt * omt
+			var omt3 = omt2 * omt
+			var t2 = t * t
+			var t3 = t2 * t
+			var x = p1.x+(p2.x-p1.x)*t
+			var np = transform_point(Vector2(x, p1.y*omt3 + yac*omt2*t*3.0 + ybc*omt*t2*3.0 + p2.y*t3))
 			draw_line(p, np, Color(1.0, 1.0, 1.0))
 			p = np
 
