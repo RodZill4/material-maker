de6543f91091aebed1af497e5504cbcafa8d1ad2
Rodz Labs <rodzilla@free.fr>
2021-07-17 09:27:55 +0200

    Added iteration attribute to the Iteration Buffer node

--- addons/material_maker/engine/gen_shader.gd
+++ addons/material_maker/engine/gen_shader.gd
@@ -274,10 +274,11 @@ func subst(string : String, context : MMGenContext, uv : String = "") -> Diction
 			if source == null:
 				continue
 			var src_attributes = source.generator.get_output_attributes(source.output_index)
-			if src_attributes.has("texture"):
-				variables[input.name+".texture"] = src_attributes.texture
-			if src_attributes.has("texture_size"):
-				variables[input.name+".size"] = src_attributes.texture_size
+			for a in src_attributes.keys():
+				if a == "texture_size":
+					variables[input.name+".size"] = src_attributes.texture_size
+				else:
+					variables[input.name+"."+a] = src_attributes[a]
 	string = replace_variables(string, variables)
 	if shader_model.has("inputs") and typeof(shader_model.inputs) == TYPE_ARRAY:
 		var cont = true
