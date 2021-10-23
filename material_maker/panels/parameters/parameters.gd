3117293d2bb87790cb4f26f6a2190de5e6b6316b
Rodz Labs <rodzilla@free.fr>
2021-10-23 16:48:51 +0200

    Updated Parameters panel with "Paint XXX" case

--- material_maker/panels/parameters/parameters.gd
+++ material_maker/panels/parameters/parameters.gd
@@ -23,11 +23,29 @@ func set_generator(g):
 		c.free()
 	controls = {}
 	if generator != null:
+		var parameter_labels = {}
 		for p in generator.get_parameter_defs():
-			var label : Label = Label.new()
-			label.text = p.label if p.has("label") else ""
-			label.size_flags_horizontal = SIZE_EXPAND_FILL
-			parameters.add_child(label)
+			if p.has("label"):
+				parameter_labels[p.label] = p
+		for p in generator.get_parameter_defs():
+			if p.has("label"):
+				if p.label.left(6) == "Paint " and parameter_labels.has(p.label.right(6)):
+					continue
+				elif parameter_labels.has("Paint "+p.label):
+					var paint_parameter = parameter_labels["Paint "+p.label]
+					var control = GENERIC.create_parameter_control(paint_parameter, false)
+					control.name = paint_parameter.name
+					if control is CheckBox:
+						control.text = p.label
+					parameters.add_child(control)
+					controls[paint_parameter.name] = control
+				else:
+					var label : Label = Label.new()
+					label.text = p.label if p.has("label") else ""
+					label.size_flags_horizontal = SIZE_EXPAND_FILL
+					parameters.add_child(label)
+			else:
+				p.add_child(Control.new())
 			var control = GENERIC.create_parameter_control(p, false)
 			control.name = p.name
 			control.size_flags_horizontal = SIZE_FILL
