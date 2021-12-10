5f33f6a43920094123ca8ff347162a458a4cc089
Rodz Labs <rodzilla@free.fr>
2021-12-10 08:45:55 +0100

    Added snap (Control) and square scale (Shift) to 2D preview parameter controls

--- material_maker/panels/preview_2d/lines.gd
+++ material_maker/panels/preview_2d/lines.gd
@@ -3,9 +3,12 @@ extends Control
 export var style : int = 1 setget set_style
 export var color : Color = Color(0.5, 0.5, 0.5) setget set_color
 
-const STYLES : Array = [ "None", "Corners", "Lines", "Grid4x4", "Grid16x16" ]
+var snap : float = 0.0
+
+const STYLES : Array = [ "None", "Corners", "Lines", "Grid4x4", "Grid10x10", "Grid16x16" ]
 
 func draw_grid(size : int) -> void:
+	snap = size
 	var parent = get_parent()
 	for i in range(size+1):
 		var x = float(i) / float(size) - 0.5
@@ -16,6 +19,7 @@ func draw_grid(size : int) -> void:
 func _draw() -> void:
 	var parent = get_parent()
 	var p
+	snap = 0.0
 	match style:
 		1:
 			p = parent.value_to_pos(Vector2(-0.5, -0.52))
@@ -39,6 +43,8 @@ func _draw() -> void:
 		3:
 			draw_grid(4)
 		4:
+			draw_grid(10)
+		5:
 			draw_grid(16)
 
 func set_style(s : int) -> void:
