09fad615ac06d57f62d6615ea3d09dacd770b045
Rodz Labs <rodzilla@free.fr>
2022-01-23 17:13:43 +0100

    Updated credits

--- material_maker/windows/about/about.gd
+++ material_maker/windows/about/about.gd
@@ -5,11 +5,12 @@ onready var authors_grid = $HBoxContainer/VBoxContainer/VBoxContainer/Authors/Li
 
 const CONTRIBUTORS = [
 	{ name="Rodolphe Suescun", contribution="Lead developer" },
-	{ name="Hugo Locurcio", contribution="Lots of contributions, mostly related to rendering and user interface" },
 	{ name="Kasper Arnklit Frandsen", contribution="Several nodes (including Auto Tones, Mask to SDF and Normal Blend) and node updates, and very nice video tutorials" },
+	{ name="Hugo Locurcio", contribution="Lots of contributions, mostly related to rendering and user interface" },
 	{ name="Roujel Williams", contribution="Curvature, Ambient Occlusion and Thickness maps generation" },
-	{ name="Bonbonmiel", contribution="Many user interface improvements (in Nodes popup, 3D preview...)" },
+	{ name="wojtekpil", contribution="Multiwarp, HBAO and Denoiser nodes as well as fixes in baker tools" },
 	{ name="GoldenThumbs", contribution="Wavefront (OBJ) model loader" },
+	{ name="Bonbonmiel", contribution="Many user interface improvements (in Nodes popup, 3D preview...)" },
 	{ name="Donald Mull Jr.", contribution="Export for Unity HDRP" },
 	{ name="Metin ÇETİN", contribution="Add node popup menu" },
 	{ name="Jack Perkins", contribution="User interface improvements" },
@@ -17,7 +18,6 @@ const CONTRIBUTORS = [
 	{ name="Jesse Dubay", contribution="3D preview and user interface improvements, Colormap node" },
 	{ name="escargot-sans-gluten", contribution="3D preview and user interface improvements" },
 	{ name="Tarox", contribution="Material Maker icon and lots of very useful feedback" },
-	{ name="wojtekpil", contribution="Multiwarp and Denoiser nodes" },
 	{ name="Easynam", contribution="Propagate Changes function in subgraph nodes" },
 	{ name="Maybe you?", contribution="If I forgot anyone here, or if you wish to contribute to this project, please don't hesitate to join our Discord channel and/or contact me directly" },
 ]
