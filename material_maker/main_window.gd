604ffed71946e8e58c41defcd43beaa54a0762b1
Rodz Labs <rodzilla@free.fr>
2022-09-17 14:40:54 +0200

    Added status bar, moved perf widgets to status bar, and added clipboard info

--- material_maker/main_window.gd
+++ material_maker/main_window.gd
@@ -39,6 +39,7 @@ onready var preview_3d_background = $VBoxContainer/Layout/SplitRight/ProjectsPan
 onready var preview_3d_background_button = $VBoxContainer/Layout/SplitRight/ProjectsPanel/PreviewUI/Preview3DButton
 onready var preview_3d_background_panel = $VBoxContainer/Layout/SplitRight/ProjectsPanel/PreviewUI/Panel
 
+
 const FPS_LIMIT_MIN = 20
 const FPS_LIMIT_MAX = 500
 const IDLE_FPS_LIMIT_MIN = 1
@@ -187,7 +188,7 @@ func _ready() -> void:
 
 	get_tree().connect("files_dropped", self, "on_files_dropped")
 
-	mm_renderer.connect("render_queue", $VBoxContainer/TopBar/RenderCounter, "on_counter_change")
+	mm_renderer.connect("render_queue", $VBoxContainer/StatusBar/RenderCounter, "on_counter_change")
 
 func _exit_tree() -> void:
 	# Save the window position and size to remember it when restarting the application
