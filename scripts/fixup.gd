c47d5dd57fb39f94b6afb841bcc05c4a6c42f7ea
Rodz Labs <rodzilla@free.fr>
2022-12-19 18:23:44 +0100

    Fixup script update (actually load/save nodes)

--- scripts/fixup.gd
+++ scripts/fixup.gd
@@ -11,11 +11,6 @@ func _run():
 		if file_name.ends_with('.mmg'):
 			print("converting "+file_name)
 			var file_path : String = "res://addons/material_maker/nodes".plus_file(file_name)
-			var file : File = File.new()
-			if file.open(file_path, File.READ) == OK:
-				var generator = loader.string_to_dict_tree(file.get_as_text())
-				file.close()
-				file.open(file_path, File.WRITE)
-				file.store_string(loader.dict_tree_to_string(generator)+"\n")
-				file.close()
+			var g = mm_loader.load_gen(file_path)
+			mm_loader.save_gen(file_path, g)
 		file_name = dir.get_next()
