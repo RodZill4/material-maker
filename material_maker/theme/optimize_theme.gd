f6ae74a878d1102536837cec33108c65ee5394c3
Rodz Labs <rodzilla@free.fr>
2023-07-11 15:40:27 +0200

    Added a script to create theme variations and updated themes

--- material_maker/theme/optimize_theme.gd
+++ material_maker/theme/optimize_theme.gd
@@ -4,13 +4,80 @@ extends EditorScript
 # This script grabs all textures from a theme and throws them into the icons
 
 func _run():
-	for t in ["res://material_maker/theme/default.tres"]:
-		optimize_theme(t)
+	create_theme_variation("res://material_maker/theme/dark.tres", "res://material_maker/theme/light.tres", invert_color, true)
+	fix_theme_geometry("res://material_maker/theme/dark.tres", "res://material_maker/theme/default.tres")
+	create_theme_variation("res://material_maker/theme/default.tres", "res://material_maker/theme/green.tres", colorize.bind(0.3))
+	create_theme_variation("res://material_maker/theme/default.tres", "res://material_maker/theme/birch.tres", colorize.bind(0.1))
+	create_theme_variation("res://material_maker/theme/default.tres", "res://material_maker/theme/mangosteen.tres", colorize.bind(0.9))
+	
+func invert_color(c : Color) -> Color:
+	return c.inverted()
+
+func colorize(c : Color, hue : float) -> Color:
+	c.h = hue
+	return c
+
+func create_theme_variation(src : String, dst : String, color_fct : Callable, invert_icons : bool = false):
+	var src_theme : Theme = load(src)
+	var dst_theme : Theme = Theme.new()
+	var references : Dictionary = {}
+	for type_name in src_theme.get_type_list():
+		#print(type_name)
+		for t in range(Theme.DATA_TYPE_MAX):
+			for theme_item_name in src_theme.get_theme_item_list(t, type_name):
+				#print("  - "+theme_item_name)
+				var src_item = src_theme.get_theme_item(t, theme_item_name, type_name)
+				var dst_item = src_item
+				if references.has(src_item):
+					dst_item = references[src_item]
+				else:
+					match t:
+						Theme.DATA_TYPE_COLOR:
+							dst_item = color_fct.call(src_item)
+						Theme.DATA_TYPE_ICON:
+							if invert_icons:
+								dst_item = load(src_item.resource_path.replace("/dark/", "/light/"))
+						Theme.DATA_TYPE_STYLEBOX:
+							dst_item = src_item.duplicate(true)
+							for p in dst_item.get_property_list():
+								if p.usage == 6 and dst_item.get(p.name) is Color:
+									dst_item.set(p.name, color_fct.call(dst_item.get(p.name)))
+					references[src_item] = dst_item
+				dst_theme.set_theme_item(t, theme_item_name, type_name, dst_item)
+	print("Saving "+dst)
+	ResourceSaver.save(dst_theme, dst)
+
+func fix_theme_geometry(src : String, dst : String):
+	var src_theme : Theme = load(src)
+	var dst_theme : Theme = load(dst)
+	var references : Dictionary = {}
+	for type_name in src_theme.get_type_list():
+		#print(type_name)
+		for t in range(Theme.DATA_TYPE_MAX):
+			for theme_item_name in src_theme.get_theme_item_list(t, type_name):
+				#print("  - "+theme_item_name)
+				var src_item = src_theme.get_theme_item(t, theme_item_name, type_name)
+				var dst_item = dst_theme.get_theme_item(t, theme_item_name, type_name)
+				if references.has(src_item):
+					dst_item = references[src_item]
+				else:
+					match t:
+						Theme.DATA_TYPE_COLOR, Theme.DATA_TYPE_ICON:
+							continue
+						Theme.DATA_TYPE_STYLEBOX:
+							var new_dst_item = src_item.duplicate(true)
+							for p in dst_item.get_property_list():
+								if p.usage == 6 and dst_item.get(p.name) is Color:
+									new_dst_item.set(p.name, dst_item.get(p.name))
+							dst_item = new_dst_item
+					references[src_item] = dst_item
+				dst_theme.set_theme_item(t, theme_item_name, type_name, dst_item)
+	print("Saving "+dst)
+	ResourceSaver.save(dst_theme, dst)
 
 func optimize_theme(theme_path):
 	# change this to share icons between themes
 	var icon_dir = theme_path.get_basename()
-	DirAccess.new().make_dir(icon_dir)
 	var theme : Theme = load(theme_path)
 	var new_theme : Theme = Theme.new()
 	for stylebox_name in theme.get_stylebox_type_list():
@@ -59,5 +126,5 @@ func optimize_theme(theme_path):
 						new_theme.set_icon(icon_name, stylebox_name, new_icon)
 			else:
 				new_theme.set_icon(icon_name, stylebox_name, new_icon)
-	ResourceSaver.save(theme_path, new_theme)
+	ResourceSaver.save(new_theme, theme_path)
 	print("done")
