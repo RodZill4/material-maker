36fa6bc1381cb696d5126d99326be8970b4ee55d
Rodz Labs <rodzilla@free.fr>
2023-10-16 21:44:57 +0200

    Changed --no-logo to --no-splash

--- parse_args.gd
+++ parse_args.gd
@@ -2,7 +2,7 @@ extends Node
 
 func _ready():
 	var args : PackedStringArray = OS.get_cmdline_args()
-	var no_logo : bool = ( args.find("--no-logo") != -1 )
+	var no_logo : bool = ( args.find("--no-splash") != -1 )
 	var scene : PackedScene
 	if no_logo:
 		scene = load("res://material_maker/main_window.tscn")
