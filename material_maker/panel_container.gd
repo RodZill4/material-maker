b32c1a696c98b9c795a9f22e07041c97360376c7
Rodz Labs <rodzilla@free.fr>
2023-11-01 10:07:51 +0100

    Switch to Godot 4.2b4 and updated panel container

--- material_maker/panel_container.gd
+++ material_maker/panel_container.gd
@@ -1,13 +1,15 @@
 extends TabContainer
 
+func _ready():
+	pass
 
 func _drop_data(_at_position, data):
-	var source : TabContainer = get_node(data.from_path)
-	var panel = source.get_tab_control(data.tabc_element)
+	var source : TabContainer = get_node(data.from_path).get_parent()
+	var panel = source.get_tab_control(data.tab_index)
 	source.remove_child(panel)
 	add_child(panel)
 
 func _can_drop_data(_at_position, data):
-	if data is Dictionary and data.has("type") and data.type == "tabc_element":
+	if data is Dictionary and data.has("type") and data.type == "tab_container_tab":
 		return true
 	return false
