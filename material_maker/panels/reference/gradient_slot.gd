6fed02dc58fbf7df92fa97a5411dc7a11560cd56
Jowan-Spooner <raban-loeffler@posteo.de>
2024-08-03 20:07:30 +0200

    Increase port-zone and fix Reference panel Gradient preview

--- material_maker/panels/reference/gradient_slot.gd
+++ material_maker/panels/reference/gradient_slot.gd
@@ -4,23 +4,26 @@ var gradient = MMGradient.new()
 
 signal clicked
 
-func _ready() -> void:
-	pass # Replace with function body.
 
 func set_gradient(g) -> void:
 	gradient = g
-	var shadertext = "shader_type canvas_item;\n"
-	shadertext += gradient.get_shader_params("my")
-	shadertext += gradient.get_shader("my")
-	shadertext += "void fragment() {\nCOLOR = my_gradient_fct(UV.x);\n}\n"
-	$ColorRect.material.shader.code = shadertext
+	var shader := ""
+	shader = "shader_type canvas_item;\n"
+	shader += gradient.get_shader_params("")
+	shader += gradient.get_shader("")
+	shader += "void fragment() { COLOR = _gradient_fct(UV.x); }"
+	$ColorRect.material.shader.code = shader
+	update_shader_parameters()
+
 
 func select(b : bool) -> void:
 	color = Color(1.0, 1.0, 1.0, 1.0) if b else Color(1.0, 1.0, 1.0, 0.0)
 
+
 func _on_ColorSlot_gui_input(event : InputEvent):
 	if event is InputEventMouseButton and event.pressed:
 		emit_signal("clicked", self)
+
 
 func _get_drag_data(_position):
 	var preview = ColorRect.new()
@@ -28,3 +31,9 @@ func _get_drag_data(_position):
 	preview.custom_minimum_size = Vector2(64, 16)
 	set_drag_preview(preview)
 	return gradient.serialize()
+
+
+func update_shader_parameters() -> void:
+	var parameter_values : Dictionary = gradient.get_parameter_values("")
+	for n in parameter_values.keys():
+		$ColorRect.material.set_shader_parameter(n, parameter_values[n])
