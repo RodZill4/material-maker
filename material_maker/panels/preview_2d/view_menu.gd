8d9b0560ebb8f797187db38277908024f68fe212
Jowan-Spooner <raban-loeffler@posteo.de>
2024-09-08 20:12:51 +0200

    Reintroduce custom grid size

--- material_maker/panels/preview_2d/view_menu.gd
+++ material_maker/panels/preview_2d/view_menu.gd
@@ -1,11 +1,15 @@
 extends PanelContainer
 
-
+var GUIDES_CUSTOM_GRID := 7
 
 func _open() -> void:
 	%ViewMode.selected = owner.get_view_mode()
 	%PostProcessing.selected = owner.get_post_processing()
 	%Guides.selected = owner.get_node("Guides").style
+	if owner.get_node("Guides").style == 1000:
+		%Guides.selected = GUIDES_CUSTOM_GRID
+	%CustomGridSize.visible = %Guides.selected == GUIDES_CUSTOM_GRID
+	%CustomGridSizeLabel.visible = %Guides.selected == GUIDES_CUSTOM_GRID
 	%GuidesColor.color = owner.get_node("Guides").color
 
 
@@ -22,8 +26,19 @@ func _on_post_processing_item_selected(index: int) -> void:
 
 
 func _on_guides_item_selected(index: int) -> void:
-	owner.get_node("Guides").style = index
+	%CustomGridSize.visible = index == GUIDES_CUSTOM_GRID
+	%CustomGridSizeLabel.visible = index == GUIDES_CUSTOM_GRID
+	if index == GUIDES_CUSTOM_GRID:
+		owner.get_node("Guides").show_grid(%CustomGridSize.get_value())
+		owner.get_node("Guides").style = 1000
+	else:
+		owner.get_node("Guides").style = index
+	size = Vector2()
 
 
 func _on_guides_color_color_changed(color: Color) -> void:
 	owner.get_node("Guides").color = color
+
+
+func _on_custom_grid_size_value_changed(value: Variant) -> void:
+	owner.get_node("Guides").show_grid(value)
