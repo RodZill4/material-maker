263cb169e288b483320260d4fc69df24336554bc
Alice NotArme <alice_marquesa@hotmail.com>
2025-03-03 13:14:12 -0300

    Refactored create_lib_dialog.gd

--- material_maker/panels/library/create_lib_dialog.gd
+++ material_maker/panels/library/create_lib_dialog.gd
@@ -1,7 +1,8 @@
 extends Window
 
-
+var file_path_line_edit: LineEdit
 var file_path = ""
+var file_name_line_edit: LineEdit
 var file_name = ""
 
 
@@ -11,34 +12,38 @@ signal return_info(status)
 func _ready():
 	$VBoxContainer/GridContainer/HBoxContainerPath/FilePickerButton.set_mode(FileDialog.FILE_MODE_OPEN_DIR)
 	$VBoxContainer/GridContainer/HBoxContainerPath/FilePickerButton.icon = get_parent().get_theme_icon("folder", "MM_Icons")
+	file_name_line_edit = $VBoxContainer/GridContainer/LineEdit
+	file_path_line_edit = $VBoxContainer/GridContainer/HBoxContainerPath/LineEdit2
 	popup_centered()
 
 func set_value(v) -> void:
-	$VBoxContainer/GridContainer/LineEdit.text = v
+	file_name_line_edit.text = v
 
 func popup_centered_(window_size : Vector2i = Vector2i(0, 0)) -> void:
 	super.popup_centered(window_size)
-	$VBoxContainer/GridContainer/LineEdit.grab_focus()
+	file_name_line_edit.grab_focus()
 
 func _on_LineEdit_text_entered(_new_text) -> void:
 	pass
 
+func validate_ok_button(fpath: String, fname: String):
+	$VBoxContainer/HBoxContainer/OK.disabled = ((fpath == "") || (fname == ""))
+
 func _on_line_edit_text_changed(new_text: String) -> void:
-	file_name = $VBoxContainer/GridContainer/LineEdit.text
-	$VBoxContainer/HBoxContainer/OK.disabled = ((file_path == "") || (file_name == ""))
+	file_name = new_text
+	validate_ok_button(file_path, file_name)
 
 func _on_line_edit_2_text_changed(new_text: String) -> void:
 	file_path = new_text
-	$VBoxContainer/HBoxContainer/OK.disabled = ((file_path == "") || (file_name == ""))
+	validate_ok_button(file_path, file_name)
 	
 func _on_FilePickerButton_file_selected(f):
 	if file_name == "":
-		$VBoxContainer/GridContainer/HBoxContainerPath/LineEdit2.text = f + "/"
+		file_path_line_edit.text = f + "/"
 	else:
-		$VBoxContainer/GridContainer/HBoxContainerPath/LineEdit2.text = f + "/" + file_name.validate_filename() + ".json"
-	file_path = $VBoxContainer/GridContainer/HBoxContainerPath/LineEdit2.text
-	$VBoxContainer/HBoxContainer/OK.disabled = ((file_path == "") || (file_name == ""))
-	
+		file_path_line_edit.text = f + "/" + file_name.validate_filename() + ".json"
+	file_path = file_path_line_edit.text
+	validate_ok_button(file_path, file_name)
 
 func _on_OK_pressed() -> void:
 	var fp
@@ -46,7 +51,7 @@ func _on_OK_pressed() -> void:
 		fp = file_path
 	else:
 		fp = file_path + "/" + file_name.validate_filename() + ".json"
-	emit_signal("return_info", { ok=true, name=$VBoxContainer/GridContainer/LineEdit.text, path=fp })
+	emit_signal("return_info", { ok=true, name=file_name, path=fp })
 
 func _on_Cancel_pressed():
 	emit_signal("return_info", { ok=false })
@@ -56,8 +61,8 @@ func _on_close_requested() -> void:
 
 func enter_info(value : String = "") -> Dictionary:
 	set_value(value)
-	$VBoxContainer/GridContainer/LineEdit.grab_focus()
-	$VBoxContainer/GridContainer/LineEdit.grab_click_focus()
+	file_name_line_edit.grab_focus()
+	file_name_line_edit.grab_click_focus()
 	popup_centered()
 	var result = await self.return_info
 	queue_free()
