b7e37a216c428b48906dd3e51152e788fa6ec0b4
LMW <williamchange@gmail.com>
2025-03-17 20:18:34 +0800

    scale fix for comment color pop-up & login dialog

--- material_maker/nodes/comment/comment.gd
+++ material_maker/nodes/comment/comment.gd
@@ -131,7 +131,10 @@ func _on_text_focus_exited():
 func _on_change_color_pressed():
 	var light_theme = "light" in mm_globals.main_window.theme.resource_path
 	accept_event()
-	$Popup.position = get_local_mouse_position()+get_screen_position()
+	var content_scale_factor = mm_globals.main_window.get_window().content_scale_factor
+	$Popup.get_window().content_scale_factor = content_scale_factor
+	$Popup.get_window().min_size = $Popup.get_window().get_contents_minimum_size() * content_scale_factor
+	$Popup.position = get_local_mouse_position() * content_scale_factor + get_screen_position()
 	$Popup.popup()
 	var corrected_color = pallette_colors.duplicate(true)
 	if !light_theme:
