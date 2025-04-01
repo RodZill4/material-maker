ed5a5abb829c56af9c107dbfdc0438eb985780b0
Rodz Labs <rodzilla@free.fr>
2025-04-01 21:36:18 +0200

    Show locale as english if it's not found

--- material_maker/windows/preferences/lang_option.gd
+++ material_maker/windows/preferences/lang_option.gd
@@ -17,6 +17,8 @@ func init_from_locales():
 func init_from_config(config : ConfigFile) -> void:
 	if config.has_section_key("config", config_variable):
 		selected = locales.find(config.get_value("config", config_variable))
+	if selected == -1:
+		selected = 0
 
 func update_config(config : ConfigFile) -> void:
 	config.set_value("config", config_variable, locales[selected])
