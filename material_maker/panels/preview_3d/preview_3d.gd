a5b7b2ff1ad445da8565e49f351c8ca7f8d3f469
LMW <williamchange@gmail.com>
2025-04-11 21:11:06 +0800

    fix exposure/white widgets in bg preview3d

--- material_maker/panels/preview_3d/preview_3d.gd
+++ material_maker/panels/preview_3d/preview_3d.gd
@@ -245,3 +245,15 @@ func do_generate_map(file_name : String, map : String, image_size : int) -> void
 	var t : MMTexture = await MMMapGenerator.get_map(object.mesh, map, image_size)
 	t.save_to_file(file_name)
 	DisplayServer.clipboard_set("{\"name\":\"image\",\"parameters\":{\"image\":\"%s\"},\"type\":\"image\"}" % file_name)
+
+
+func _on_exposure_value_changed(value: Variant) -> void:
+	var environment = $MaterialPreview/Preview3d/WorldEnvironment.environment
+	environment.tonemap_exposure = value
+	mm_globals.set_config("ui_3d_preview_tonemap_exposure", value)
+
+
+func _on_white_value_changed(value: Variant) -> void:
+	var environment = $MaterialPreview/Preview3d/WorldEnvironment.environment
+	environment.tonemap_white = value
+	mm_globals.set_config("ui_3d_preview_tonemap_white", value)
