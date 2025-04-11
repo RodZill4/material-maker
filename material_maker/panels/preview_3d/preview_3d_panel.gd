1e51c79fa47cf6f20a9f5d33449b6da0289f2a58
LMW <williamchange@gmail.com>
2025-04-11 12:23:01 +0800

    reset center for shift + mmb offsets in 3d view

--- material_maker/panels/preview_3d/preview_3d_panel.gd
+++ material_maker/panels/preview_3d/preview_3d_panel.gd
@@ -44,9 +44,11 @@ func on_right_click():
 
 func _on_PopupMenu_id_pressed(id):
 	var pivot = get_node("MaterialPreview/Preview3d/ObjectsPivot/Objects")
+	var cam_control = get_node("MaterialPreview/Preview3d/CameraController")
 	match id:
 		0:
 			pivot.transform.origin = Vector3(0, 0, 0)
+			cam_control.transform.origin = Vector3(0, 0, 0)
 		1:
 			pivot.transform.origin = new_pivot_position
 
