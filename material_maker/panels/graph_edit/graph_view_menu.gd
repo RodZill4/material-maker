06d5ca982716c679fc244a8a802c236844b9e90b
LMW <williamchange@gmail.com>
2025-04-14 18:26:06 +0800

    Graph: connection line thickness/curvature option

--- material_maker/panels/graph_edit/graph_view_menu.gd
+++ material_maker/panels/graph_edit/graph_view_menu.gd
@@ -4,8 +4,8 @@ const SETTING_GRAPH_MINIMAP := "graph_minimap"
 const SETTING_GRAPH_GRID_VISIBILITY := "graph_grid_visibility"
 const SETTING_GRAPH_GRID_SIZE := "graph_grid_size"
 const SETTING_GRAPH_GRID_SNAPPING := "graph_grid_snapping"
-
-
+const SETTING_GRAPH_LINE_CURVATURE := "graph_line_curvaature"
+const SETTING_GRAPH_LINE_THICKNESS := "graph_line_thickness"
 
 func _ready() -> void:
 	if mm_globals.has_config(SETTING_GRAPH_MINIMAP):
@@ -19,6 +19,12 @@ func _ready() -> void:
 
 	if mm_globals.has_config(SETTING_GRAPH_GRID_SNAPPING):
 		%GridSnapping.button_pressed = mm_globals.get_config(SETTING_GRAPH_GRID_SNAPPING)
+	
+	if mm_globals.has_config(SETTING_GRAPH_LINE_CURVATURE):
+		%LineCurvature.value = mm_globals.get_config(SETTING_GRAPH_LINE_CURVATURE)
+	
+	if mm_globals.has_config(SETTING_GRAPH_LINE_THICKNESS):
+		%LineThickness.value = mm_globals.get_config(SETTING_GRAPH_LINE_THICKNESS)
 
 
 	%Projects.tab_changed.connect(update_view_settings)
@@ -47,6 +53,16 @@ func _on_grid_size_value_changed(value: Variant) -> void:
 	update_view_settings()
 
 
+func _on_line_curvature_value_changed(value: Variant) -> void:
+	mm_globals.set_config(SETTING_GRAPH_LINE_CURVATURE, value)
+	update_view_settings()
+
+
+func _on_line_thickness_value_changed(value: Variant) -> void:
+	mm_globals.set_config(SETTING_GRAPH_LINE_THICKNESS, value)
+	update_view_settings()
+
+
 func update_view_settings(_arg_ignore:Variant = null) -> void:
 	for n in %Projects.get_children():
 		var graph: GraphEdit = null
@@ -60,3 +76,5 @@ func update_view_settings(_arg_ignore:Variant = null) -> void:
 		graph.show_grid = %GridVisibility.button_pressed
 		graph.snapping_distance = %GridSize.get_value()
 		graph.snapping_enabled = %GridSnapping.button_pressed
+		graph.connection_lines_curvature = %LineCurvature.value
+		graph.connection_lines_thickness = %LineThickness.value
