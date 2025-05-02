351382f4984b5538fe7803ca523ed094b0c67bf5
LMW <williamchange@gmail.com>
2025-05-03 00:11:51 +0800

    Add sdf2d arrow

--- addons/material_maker/sdf_builder/sdf2d/shapes/pie.gd
+++ addons/material_maker/sdf_builder/sdf2d/shapes/pie.gd
@@ -7,15 +7,14 @@ func get_parameter_defs():
 	return [
 		{ label="Position.x", name="position_x", type="float", min=-1.0, max=1.0, step=0.01, default=0.0, control="P1.x" },
 		{ label="Position.y", name="position_y", type="float", min=-1.0, max=1.0, step=0.01, default=0.0, control="P1.y" },
-		{ label="Radius", name="r", type="float", min=0.0, max=1.0, step=0.01, default=0.15, control="Radius1.r"},
-		{ label="Width", name="bx", type="float", min=0.0, max=1.0, step=0.01, default=0.5, control="Rect1.x"},
-		{ label="Height", name="by", type="float", min=0.0, max=1.0, step=0.01, default=0.15, control="Rect1.y"},
+		{ label="Radius", name="r", type="float", min=0.0, max=1.0, step=0.01, default=0.5, control="Radius1.r"},
+		{ label="Aperture", name="aa", type="float", min=-90.0, max=90.0, step=1.0, default=-45, control="Radius1.a"},
 		{ label="Rotation", name="angle", type="float", min=-180.0, max=180.0, step=0.01, default=0.0, control="RotateScale1.a" },
 		{ label="Scale", name="scale", type="float", min=-1.0, max=1.0, step=0.01, default=1.0, control="RotateScale1.r" },
 	]
 
 func get_includes():
-	return [ "sdcross", "rotate" ]
+	return [ "sdpie", "rotate" ]
 
 func shape_code(scene : Dictionary, uv : String = "$uv") -> String:
-	return "sdCross(%s, vec2($bx, $by) + $r, $r)" % [ uv ]
+	return "sdPie(%s, vec2(sin(radians(abs(mod(450.0 + $aa , 360.0) - 180.0))), cos(radians(abs(mod(450.0 + $aa , 360.0) - 180.0)))), $r)" % [ uv ]
