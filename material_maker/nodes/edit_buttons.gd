ed2f7fea8fca728efcdfc525116a4c22a62e5873
LMW <williamchange@gmail.com>
2025-05-10 23:25:05 +0800

    Fix node edit buttons colors on light theme

--- material_maker/nodes/edit_buttons.gd
+++ material_maker/nodes/edit_buttons.gd
@@ -4,3 +4,8 @@ func connect_buttons(object, edit_fct, load_fct, save_fct) -> void:
 	$Edit.connect("pressed", Callable(object, edit_fct))
 	$Load.connect("pressed", Callable(object, load_fct))
 	$Save.connect("pressed", Callable(object, save_fct))
+
+func _on_ready() -> void:
+	$Edit.add_theme_color_override("icon_normal_color", Color.WHITE)
+	$Load.add_theme_color_override("icon_normal_color", Color.WHITE)
+	$Save.add_theme_color_override("icon_normal_color", Color.WHITE)
