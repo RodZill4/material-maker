fb60bd788c5650dc65dd23e423f503c72e99c7f4
LMW <williamchange@gmail.com>
2025-08-07 11:43:21 +0800

    Improve curve editor

--- material_maker/widgets/curve_edit/slope_point.gd
+++ material_maker/widgets/curve_edit/slope_point.gd
@@ -3,6 +3,8 @@ extends Control
 @export var distance : float
 
 var moving = false
+var hovering : bool = false
+var is_selected : bool = false
 
 const OFFSET = -Vector2(0, 0)
 
@@ -12,12 +14,17 @@ func _ready():
 func _draw():
 	var current_theme : Theme = mm_globals.main_window.theme
 	var color : Color = current_theme.get_color("font_color", "Label")
-	draw_circle(Vector2(3.0, 3.0), 3.0, color)
+	var selected_color : Color = current_theme.get_color("icon_pressed_color", "Button")
+	draw_circle(custom_minimum_size*0.5, 3.0, selected_color if is_selected else color)
+	if hovering or is_selected:
+		draw_circle(custom_minimum_size*0.5, 6.0, color, false, 0.5, true)
 
 func _on_ControlPoint_gui_input(event : InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
+				is_selected = true
+				queue_redraw()
 				if event.double_click:
 					var parent = get_parent()
 					var vector : Vector2
@@ -27,18 +34,30 @@ func _on_ControlPoint_gui_input(event : InputEvent):
 						vector = parent.get_parent().get_child(parent.get_index()+1).position-parent.position
 					vector = distance*vector.normalized()
 					position = vector-OFFSET
-					if event.is_control_or_command_pressed():
+					if not event.shift_pressed:
 						get_parent().get_child(1-get_index()).position = -vector-OFFSET
 					get_parent().update_tangents()
 				else:
 					moving = true
 			else:
+				is_selected = false
+				queue_redraw()
 				moving = false
 	elif moving and event is InputEventMouseMotion:
 		var vector = get_global_mouse_position()-get_parent().get_global_rect().position+OFFSET
 		vector *= sign(vector.x)
 		vector = distance*vector.normalized()
 		position = vector-OFFSET
-		if event.is_command_or_control_pressed():
+		if not event.shift_pressed:
 			get_parent().get_child(1-get_index()).position = -vector-OFFSET
 		get_parent().update_tangents()
+
+
+func _on_mouse_entered() -> void:
+	hovering = true
+	queue_redraw()
+
+
+func _on_mouse_exited() -> void:
+	hovering = false
+	queue_redraw()
