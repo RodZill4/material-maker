b5ae0114ce2e76f6aaa6bd30b12699ff6000b302
LMW <williamchange@gmail.com>
2025-12-15 23:16:52 +0800

    Fixed opaque bg for tooltip/popup menu panels

--- material_maker/main_window.gd
+++ material_maker/main_window.gd
@@ -623,7 +623,7 @@ func change_theme(theme_name) -> void:
 		RenderingServer.set_default_clear_color(Color(0.14, 0.17,0.23))
 	else:
 		RenderingServer.set_default_clear_color(
-				Color(0.48, 0.48, 0.48) if "light" in theme_name else Color(0.12, 0.12, 0.12))
+				Color("4d4d4d") if "light" in theme_name else Color("1f1f1f"))
 	$NodeFactory.on_theme_changed()
 
 func _on_SetTheme_id_pressed(id) -> void:
