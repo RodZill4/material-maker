359daaaa36195b475ab33b8bc5e8e83169ccd85b
LMW <williamchange@gmail.com>
2026-03-09 01:34:31 +0800

    added 2d sdf preview customization

--- material_maker/windows/preferences/color_option.gd
+++ material_maker/windows/preferences/color_option.gd
@@ -1,11 +1,13 @@
-class_name  ColorOption
-
+class_name ColorOption
 extends ColorPickerButton
 
 @export var config_variable : String
 
 func _ready() -> void:
-	pass
+	get_popup().about_to_popup.connect(_picker_about_to_popup)
+
+func _picker_about_to_popup() -> void:
+	get_popup().content_scale_factor = get_tree().root.content_scale_factor
 
 func init_from_config(config : ConfigFile) -> void:
 	if config.has_section_key("config", config_variable):
