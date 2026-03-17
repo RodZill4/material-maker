7dd2c4492e35434841dfb2e6c46278cd5a1c0160
LMW <williamchange@gmail.com>
2026-03-17 22:11:37 +0800

    pref constants

--- material_maker/windows/preferences/shaders/preference_preview_sdf2d.gd
+++ material_maker/windows/preferences/shaders/preference_preview_sdf2d.gd
@@ -1,6 +1,16 @@
 class_name Sdf2DPreview
 extends ColorRect
 
+const SETTING_SDF2D_BG_COLOR := "ui_preview_sdf2d_background_color"
+const SETTING_SDF2D_FG_COLOR_1 := "ui_preview_sdf2d_foreground_color1"
+const SETTING_SDF2D_FG_COLOR_2 := "ui_preview_sdf2d_foreground_color2"
+const SETTING_SDF2D_BORDER_THICKNESS := "ui_preview_sdf2d_border_thickness"
+const SETTING_SDF2D_ISOLINE_DENSITY := "ui_preview_sdf2d_isoline_density"
+const SETTING_SDF2D_ISOLINE_THICKNESS := "ui_preview_sdf2d_isoline_thickness"
+const SETTING_SDF2D_ISOLINE_OPACITY := "ui_preview_sdf2d_isoline_opacity"
+const SETTING_SDF2D_ISOLINE_SMOOTH := "ui_preview_sdf2d_isoline_smoothness"
+const SETTING_SDF2D_SHADOW_OPACITY := "ui_preview_sdf2d_shadow_opacity"
+
 func _on_ready() -> void:
 	await get_tree().process_frame
 	material.set_shader_parameter("bg", %BgColor.color)
@@ -14,16 +24,16 @@ func _on_ready() -> void:
 	material.set_shader_parameter("sha", %ShadowFac.value)
 
 static func get_shader_code(preview_code : String) -> String:
-	const vec3_str = "vec3(%s, %s, %s)"
-	var bg : Color = mm_globals.get_config("ui_preview_sdf2d_background_color")
-	var fg1 : Color = mm_globals.get_config("ui_preview_sdf2d_foreground_color1")
-	var fg2 : Color = mm_globals.get_config("ui_preview_sdf2d_foreground_color2")
-	var border_thickness = mm_globals.get_config("ui_preview_sdf2d_border_thickness")
-	var isoline_density = mm_globals.get_config("ui_preview_sdf2d_isoline_density")
-	var isoline_thickness = mm_globals.get_config("ui_preview_sdf2d_isoline_thickness")
-	var isoline_opacity = mm_globals.get_config("ui_preview_sdf2d_isoline_opacity")
-	var isoline_smoothness = mm_globals.get_config("ui_preview_sdf2d_isoline_smoothness")
-	var shadow_opacity = mm_globals.get_config("ui_preview_sdf2d_shadow_opacity")
+	const vec3_str : String = "vec3(%s, %s, %s)"
+	var bg : Color = mm_globals.get_config(SETTING_SDF2D_BG_COLOR)
+	var fg1 : Color = mm_globals.get_config(SETTING_SDF2D_FG_COLOR_1)
+	var fg2 : Color = mm_globals.get_config(SETTING_SDF2D_FG_COLOR_2)
+	var border_thickness : float = mm_globals.get_config(SETTING_SDF2D_BORDER_THICKNESS)
+	var isoline_density : float = mm_globals.get_config(SETTING_SDF2D_ISOLINE_DENSITY)
+	var isoline_thickness : float = mm_globals.get_config(SETTING_SDF2D_ISOLINE_THICKNESS)
+	var isoline_opacity : float = mm_globals.get_config(SETTING_SDF2D_ISOLINE_OPACITY)
+	var isoline_smoothness : float = mm_globals.get_config(SETTING_SDF2D_ISOLINE_SMOOTH)
+	var shadow_opacity : float = mm_globals.get_config(SETTING_SDF2D_SHADOW_OPACITY)
 	preview_code = preview_code.replace("$(bg)",  vec3_str % [bg.r, bg.g, bg.b])
 	preview_code = preview_code.replace("$(fg1)", vec3_str % [fg1.r, fg1.g, fg1.b])
 	preview_code = preview_code.replace("$(fg2)", vec3_str % [fg2.r, fg2.g, fg2.b])
@@ -36,15 +46,15 @@ static func get_shader_code(preview_code : String) -> String:
 	return preview_code
 
 func load_defaults() -> void:
-	%BgColor.color = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_background_color"]
-	%FgColor1.color = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_foreground_color1"]
-	%FgColor2.color = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_foreground_color2"]
-	%BorderThickness.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_border_thickness"]
-	%IsolineDensity.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_isoline_density"]
-	%IsolineThickness.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_isoline_thickness"]
-	%IsolineOpacity.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_isoline_opacity"]
-	%IsolineSmoothness.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_isoline_smoothness"]
-	%ShadowFac.value = mm_globals.DEFAULT_CONFIG["ui_preview_sdf2d_shadow_opacity"]
+	%BgColor.color = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_BG_COLOR]
+	%FgColor1.color = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_FG_COLOR_1]
+	%FgColor2.color = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_FG_COLOR_2]
+	%BorderThickness.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_BORDER_THICKNESS]
+	%IsolineDensity.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_ISOLINE_DENSITY]
+	%IsolineThickness.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_ISOLINE_THICKNESS]
+	%IsolineOpacity.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_ISOLINE_OPACITY]
+	%IsolineSmoothness.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_ISOLINE_SMOOTH]
+	%ShadowFac.value = mm_globals.DEFAULT_CONFIG[SETTING_SDF2D_SHADOW_OPACITY]
 	_on_ready()
 
 func _on_border_thickness_value_changed(value: Variant) -> void:
@@ -79,7 +89,7 @@ func _on_reset_button_pressed() -> void:
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var f = lerp(0.0, 0.3, event.position.x / size.x)
+		var f : float = lerpf(0.0, 0.3, event.position.x / size.x)
 		material.set_shader_parameter("preview_sdf_radius", f)
 
 func _on_mouse_exited() -> void:
